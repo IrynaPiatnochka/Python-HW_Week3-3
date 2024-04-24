588edc098b14c8f58c3ae8d645664ccdeb4ac240
Iryna-Piatnochka <ivpiatnochka@gmail.com>
2024-04-23 22:46:50 -0500

    updated code

--- 2_Set Operations in Data Analysis.py
+++ 2_Set Operations in Data Analysis.py
@@ -2,13 +2,12 @@
 # Create a function for data analysis of customers id's.
 
 def id_set():
+    
     customer_ids = ["C001", "C002", "C003", "C002", "C001", "C004"]
     # print each id separately and without a duplicate.
     for id in set(customer_ids):
         print(id)
     # return a set of unique id's.
-    set_list = set(customer_ids)
-    customer_ids = list(set_list)
-    print(customer_ids)
+    print(set(customer_ids))
 
 id_set()
